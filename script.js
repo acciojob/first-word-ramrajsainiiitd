1fc76e8d2f585817b63c4fd26da44aaee3862e3b
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-13 22:47:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
-  // your code here
+  	// your code here
+	  let trimedWord = s.trim().split(" ");
+	  if (s.length==0) {
+		  return '';
+	  }
+	  else{
+		return trimedWord[0];
+	  }
 }
 
-// Do not change the code below
-
+// Do not change the code belowr
 const s = prompt("Enter String:");
 alert(firstWord(s));
